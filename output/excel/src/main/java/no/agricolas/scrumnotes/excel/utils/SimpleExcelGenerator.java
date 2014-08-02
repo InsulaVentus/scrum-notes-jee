8d0f8d0714f0fc8d7cb0a777672f759d78a83a8d
Simen Sohol <simen.so@outlook.com>
2014-08-03 00:19:19 +0200

    Updated pom-files and added jxl-dependency in the output-module

---
+++ output/excel/src/main/java/no/agricolas/scrumnotes/excel/utils/SimpleExcelGenerator.java
@@ -0,0 +1,9 @@
+package java.no.agricolas.scrumnotes.excel.utils;
+
+/**
+ * This class will generate a simple excelfile to a given location
+ * @author Simen Søhol
+ */
+public class SimpleExcelGenerator {
+
+}
